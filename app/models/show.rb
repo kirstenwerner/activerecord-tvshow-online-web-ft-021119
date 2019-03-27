89fb59f05c5947ca29a51622ccb9c71652223a40
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-27 20:11:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.order(:rating).reverse.first
+    self.order(:rating).last
   end 
 
   def self.lowest_rating 
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end 
 
   def self.least_popular_show 
-  
+    self.order(:rating).first
   end 
 
   def self.ratings_sum 
