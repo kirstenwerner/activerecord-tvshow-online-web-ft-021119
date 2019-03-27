2ef02d701cb6817fca3ca417f75ff8227c0b48e8
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-27 20:10:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base 
   
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
   
   def self.most_popular_show
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
   end 
 
   def self.lowest_rating 
-  
+    self.minimum(:rating)
   end 
 
   def self.least_popular_show 
